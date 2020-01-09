b6f314a8d4a4041ddf99f1b23b50b927e255123f
mndangee <38105502+mndangee@users.noreply.github.com>
2020-01-10 03:55:07 +0900

    Delete main.js

--- js/main.js
+++
@@ -1,8 +0,0 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 6,
-    centeredSlides: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-      },  
-});
